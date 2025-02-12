2fc922f67ea2824fc130fd6808b20e3f94bde747
warley farias <157419864+warrley@users.noreply.github.com>
2025-02-12 13:49:26 -0300

    add array line

---
+++ vectors I/arrayLine/README.md
@@ -0,0 +1,26 @@
+# 🖩 Array em uma linha
+
+Este é um programa para imprimir os valores de um array de numeros entre colchetes.
+
+---
+
+## 🔧 **Pré-requisitos**
+
+Antes de usar o programa, certifique-se de ter um compilador C instalado.
+
+# **Executar**
+
+**1** Vá no cmd e navegue até a pasta do codigo
+
+**2** Escreva arrayLine.exe
+
+# **Exemplo**
+
+How many items? 3
+
+Enter the 1 item 1
+
+Enter the 2 item 3
+
+Enter the 3 item 4
+[ 1 3 4 ]
---
+++ vectors I/arrayLine/arrayLine.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <math.h>
+
+// https://github.com/qxcodefup/arcade/blob/master/base/vet_i/Readme.md
+
+int main() {
+    int length;
+    int items[1000];
+
+    printf("\nHow many items? ");
+    scanf("%d", &length);
+    
+    for(int i = 0; i < length; i++) {
+        printf("\nEnter the %d item ", i + 1);
+        scanf("%d", &items[i]);
+    }
+    printf("[");
+    for(int i = 0; i < length; i++) {
+        printf(" %d", items[i]);
+    }
+    printf(" ]");
+    return 0;
+}
